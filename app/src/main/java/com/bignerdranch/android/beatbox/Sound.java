7460bba547a4ef8240396c0e1059e5b2d175ffab
Jeff Corpac <jcorpac@gmail.com>
2016-02-23 15:03:56 -0600

    Chapter 18 Complete - Added BeatBox and Sound model - Connected Beatbox to the RecyclerView

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.beatbox;
+
+/**
+ * Created by Jeff on 2/23/2016.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String fileName = components[components.length - 1];
+        mName = fileName.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
