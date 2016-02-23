3196409e9044f8e63682d2062926869918e43e1a
Jeff Corpac <jcorpac@gmail.com>
2016-02-23 14:10:18 -0600

    Initial Commit - Framework setup

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.beatbox;
+
+/**
+ * Created by Jeff on 2/23/2016.
+ */
+public class BeatBox {
+    private static final String LOG_TAG = "BeatBox";
+
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+}
